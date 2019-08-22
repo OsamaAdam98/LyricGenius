c0ae908270ff8a97e652e194221a6ce7c4043dbe
Osama Adam <osamaadam98@gmail.com>
2019-08-22 16:13:50 +0200

    removed axios and using fetch instead

--- src/components/layout/HomePage.js
+++ src/components/layout/HomePage.js
@@ -1,5 +1,4 @@
 import React, {useState, useEffect} from "react";
-import axios from "axios";
 
 import TrackCard from "../TrackCard";
 import LoadingPage from "./LoadingPage";
@@ -8,16 +7,25 @@ export default function HomePage() {
 	const [trackList, setTrackList] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
 
+	const url = `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&apikey=${
+		process.env.REACT_APP_KEY
+	}`;
+
 	useEffect(() => {
 		setIsLoading(true);
+		/*
 		axios
-			.get(
-				`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&apikey=${
-					process.env.REACT_APP_KEY
-				}`
-			)
+			.get(url)
 			.then((res) => {
 				setTrackList(res.data.message.body.track_list);
+				setIsLoading(false);
+			})
+			.catch((err) => console.log(err));
+			*/
+		fetch(url)
+			.then((res) => res.json())
+			.then((resData) => {
+				setTrackList(resData.message.body.track_list);
 				setIsLoading(false);
 			})
 			.catch((err) => console.log(err));

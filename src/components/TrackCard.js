04705af80b4c53eb10be90d1990cabd0f32c7ed3
Osama Adam <osamaadam98@gmail.com>
2019-08-22 16:13:31 +0200

    deconstructing props

--- src/components/TrackCard.js
+++ src/components/TrackCard.js
@@ -1,17 +1,18 @@
 import React from "react";
 
 export default function TrackCard(props) {
+	const {track} = props;
 	return (
 		<div className="col-md-6 mb-3">
 			<div className="card shadow-sm">
 				<div className="card-body ">
-					<h5 className="display-4">{props.track.track_name}</h5>
+					<h5 className="display-4">{track.track_name}</h5>
 					<ul className="list-group list-group-flush">
 						<li className="list-group-item pl-0">
-							<strong>Album name</strong>: {props.track.album_name}
+							<strong>Album name</strong>: {track.album_name}
 						</li>
 						<li className="list-group-item pl-0">
-							<strong>Artist</strong>: {props.track.artist_name}
+							<strong>Artist</strong>: {track.artist_name}
 						</li>
 					</ul>
 				</div>

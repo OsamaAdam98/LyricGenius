73cdbc57dda8cf383a9adda614cb9f5b1ef3627a
Osama Adam <osamaadam98@gmail.com>
2019-08-22 05:43:57 +0200

    nightly commit

--- src/components/TrackCard.js
+++ src/components/TrackCard.js
@@ -2,19 +2,20 @@ import React from "react";
 
 export default function TrackCard(props) {
 	return (
-		<div className="container-fluid">
-			<div className="card">
-				<div className="card-body">
-					<h5 className="card-title">{props.track.track_name}</h5>
+		<div className="col-md-6 mb-3">
+			<div className="card shadow-sm">
+				<div className="card-body ">
+					<h5 className="display-4">{props.track.track_name}</h5>
+					<ul className="list-group list-group-flush">
+						<li className="list-group-item pl-0">
+							<strong>Album name</strong>: {props.track.album_name}
+						</li>
+						<li className="list-group-item pl-0">
+							<strong>Artist</strong>: {props.track.artist_name}
+						</li>
+					</ul>
 				</div>
-				<ul className="list-group list-group-flush">
-					<li className="list-group-item">
-						Album name: {props.track.album_name}
-					</li>
-					<li className="list-group-item">Artist: {props.track.artist_name}</li>
-				</ul>
 			</div>
-			<br />
 		</div>
 	);
 }

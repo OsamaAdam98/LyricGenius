513473d2cf2f0afa0656db19453af283732dc9ae
Osama Adam <osamaadam98@gmail.com>
2019-08-25 21:47:43 +0200

    fully working search function

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavBar from "./components/layout/NavBar";
 import Homepage from "./components/layout/Homepage";
 import About from "./components/layout/About";
 import Lyrics from "./components/layout/Lyrics";
+import Searchpage from "./components/layout/Searchpage";
 
 import "./App.css";
 
@@ -15,7 +16,8 @@ function App() {
 				<Switch>
 					<Route exact path="/" component={Homepage} />
 					<Route path="/about/" component={About} />
-					<Route exact path = "/lyrics/:id" component={Lyrics}/>
+					<Route exact path="/lyrics/:id" component={Lyrics} />
+					<Route path="/inquiry/:trackName" component={Searchpage} />
 				</Switch>
 			</div>
 		</Router>

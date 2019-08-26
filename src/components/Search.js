ee972ae0d2a94cc775129a8ec4c9f07cdd2fc7e5
Osama Adam <osamaadam98@gmail.com>
2019-08-26 03:06:27 +0200

    solved a weird conflict

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, {useState} from "react";
+import {withRouter} from "react-router-dom";
 
-export default function Search(props) {
-  return (
-    <div>
+function Search(props) {
+	const [searchState, setSearchState] = useState("");
 
-    </div>
-  )
+	const direction = `/inquiry/${searchState}`;
+
+	const handleChange = (event) => {
+		const {value} = event.target;
+		setSearchState(value);
+	};
+
+	const handleSubmit = (event) => {
+		if (searchState.trim()) props.history.push(direction);
+		else {
+			event.preventDefault();
+		}
+	};
+
+	return (
+		<React.Fragment>
+			<form className="form-inline" onSubmit={handleSubmit}>
+				<input
+					className={`form-control mr-sm-2`}
+					type="search"
+					placeholder="Search.."
+					value={searchState}
+					onChange={handleChange}
+				/>
+				<button
+					onClick={handleSubmit}
+					className={`form-control btn btn-outline-light`}
+				>
+					Search
+				</button>
+			</form>
+		</React.Fragment>
+	);
 }
+
+export default withRouter(Search);
